9619da568a48b7fc2083ce44221d12e616349a0d
smter <smtersoyoglu231@gmail.com>
2023-09-18 14:28:06 +0300

    Güncel

--- app/src/main/java/com/sametersoyoglu/kotlinbootcamp/MainActivity.kt
+++ app/src/main/java/com/sametersoyoglu/kotlinbootcamp/MainActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     // lateinit  daha sonra başlatacağım anlamına gelir
-    lateinit var str2 : String
+    //lateinit var str2 : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Yazılım kısmında R harfi res klasörünü temsil eder.
+        // Xml kısmında @ işareti res klasörünü temsil eder.
 
-        str2 = "Merhaba"
+        // Tasarımda bu kavramları duyarız
+        // tasarımın solu : Left - Start - Leading
+        // sağı : Right - End - Trailing   -> bu isimlerle çıkabilirler.
+        // Buton un arkaplan rengini backgroundTint ile değiştiririz.
 
     }
 }
